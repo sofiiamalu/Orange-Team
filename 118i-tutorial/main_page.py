ed73c424dadf8f79084272a42c4412e8a82e7b67
sofiiamalu <149350468+sofiiamalu@users.noreply.github.com>
2024-03-27 21:31:49 -0700

    Add files via upload

---
+++ 118i-tutorial/main_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+message = "Welcome to the main page! 🎉\nPlease navigate the pages to explore some sample code."
+st.write(message)
